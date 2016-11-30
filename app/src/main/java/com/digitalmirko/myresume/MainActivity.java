93c9f334fc347f1c92c670ca83f39ead8f461b43
Mirko Cukich <mirkocukich@gmail.com>
2016-11-30 15:50:36 -0600

    Added Call, Email and Samples Website button code

--- app/src/main/java/com/digitalmirko/myresume/MainActivity.java
+++ app/src/main/java/com/digitalmirko/myresume/MainActivity.java
@@ -1,6 +1,7 @@
 package com.digitalmirko.myresume;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button summaryButton;
-    Button experienceButton;
+    // Buttons used
+    Button summaryBtn;
+    Button experienceBtn;
+    Button callBtn;
+    Button emailBtn;
+    Button webBtn;
 
 
     @Override
@@ -17,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        summaryButton = (Button) findViewById(R.id.summaryBtn);
-        summaryButton.setOnClickListener(new View.OnClickListener() {
+        // Summary Button (Summary)
+        summaryBtn = (Button) findViewById(R.id.summaryBtn);
+        summaryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent goToTheOtherActivity = new Intent(getApplicationContext(),SummaryActivity.class);
@@ -26,13 +32,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        experienceButton = (Button) findViewById(R.id.experienceBtn);
-        experienceButton.setOnClickListener(new View.OnClickListener() {
+        // Experience Button (Experience)
+        experienceBtn = (Button) findViewById(R.id.experienceBtn);
+        experienceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent goToTheOtherActivity = new Intent(getApplicationContext(),ExperienceActivity.class);
                 startActivity(goToTheOtherActivity);
+            }
+        });
+
+        // Phone Call Button (Call Mirko)
+        callBtn = (Button) findViewById(R.id.callBtn);
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri myPhoneNumber = Uri.parse("tel:18001234567");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, myPhoneNumber);
+                startActivity(callIntent);
+            }
+        });
+
+        // Send Email Button (Email Mirko)
+        emailBtn = (Button) findViewById(R.id.emailBtn);
+        emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mirko@digitalmirko.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mirko's Resume");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "I really like your resume!. Let's talk.");
+                startActivity(emailIntent);
+            }
+        });
+
+        // Goto Web URL Button (Samples Website)
+        webBtn = (Button) findViewById(R.id.webBtn);
+        webBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent webIntent = new Intent(Intent.ACTION_VIEW);
+                webIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+                webIntent.setData(Uri.parse("http://www.digitalmirko.com"));
+                startActivity(webIntent);
             }
         });
     }

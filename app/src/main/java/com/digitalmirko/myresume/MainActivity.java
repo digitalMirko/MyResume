dbf0eb3f738a60b9271d01da8b4e5ae0059cb1b3
Mirko Cukich <mirkocukich@gmail.com>
2016-11-30 14:11:50 -0600

    Added Scrollable Summary with text

--- app/src/main/java/com/digitalmirko/myresume/MainActivity.java
+++ app/src/main/java/com/digitalmirko/myresume/MainActivity.java
@@ -1,13 +1,28 @@
 package com.digitalmirko.myresume;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button summaryButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        summaryButton = (Button) findViewById(R.id.summaryBtn);
+        summaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToTheOtherActivity = new Intent(getApplicationContext(),SummaryActivity.class);
+                startActivity(goToTheOtherActivity);
+            }
+        });
     }
 }
